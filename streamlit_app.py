d4a7bf52ff6e8cb94f3aec67382e89a2fda818c8
cudmora <a@sabre-it.com>
2022-10-02 15:19:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
-
-streamlit.title('Menu')
-streamlit.text('omega')
-streamlit.text('Kale')
-streamlit.text('Kal1e')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 
